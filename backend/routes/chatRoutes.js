889ad8ec4f6bc274baa348d75f6156ed63b29652
Vedant Dandge <vedantdandgepro@gmail.com>
2022-11-16 23:04:02 +0530

    chat api done

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const {accessChat, fetchChats} = require("../controllers/chatControllers")
+const {accessChat, fetchChats, createGroupChat, renameGroup, addToGroup,removeFromGroup} = require("../controllers/chatControllers")
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/remove").put(protect, removeFromGroup);
-// router.route("groupadd").put(protect, addToGroup);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/remove").put(protect, removeFromGroup);
+router.route("groupadd").put(protect, addToGroup);
 
 module.exports = router;
