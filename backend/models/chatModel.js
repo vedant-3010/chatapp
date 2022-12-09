769e004da08104d849f88a1848c4ee5798b84f97
Vedant Dandge <vedantdandgepro@gmail.com>
2022-12-09 10:23:52 +0530

    minor

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -4,7 +4,7 @@ const chatModel = mongoose.Schema(
   {
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
-    user: [
+    users: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
